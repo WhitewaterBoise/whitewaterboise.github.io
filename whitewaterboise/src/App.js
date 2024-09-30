74802bff63ed806604d349fa7b2d2ff4dd733e38
owendabestone <58024709+owendabestone@users.noreply.github.com>
2024-09-29 19:53:02 -0700

    uhhh

---
+++ whitewaterboise/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import PageNavbar from "./components/Navbar"
+function App() {
+  return (
+    <div className="App">
+      <PageNavbar>
+
+      </PageNavbar>
+hi
+    </div>
+  );
+}
+
+export default App;
