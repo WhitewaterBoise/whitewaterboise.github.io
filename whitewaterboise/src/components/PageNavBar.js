5826bbe0d7e78eff798209c8a55fb2fa3483aa0f
Nina-Eld <chang.owen.zhang@gmail.com>
2025-04-12 14:31:32 -0600

    brand logo hot fix

--- whitewaterboise/src/components/PageNavBar.js
+++ whitewaterboise/src/components/PageNavBar.js
@@ -7,7 +7,7 @@ export default function PageNavBar() {
       <Navbar.Brand href="/" >
         <img style={{filter: " saturate(1.2)"}} src={logo} className="mr-1 sm:mr-3 h-9" alt="Whitewater TMS logo" />
 
-        <span className=" self-center whitespace-nowrap text-xl font-semibold dark:text-white " style={{ fontFamily: "Poiret One", letterSpacing: -1 }}>WHITEWATER TMS</span>
+        <span className=" self-center whitespace-nowrap text-xl font-semibold dark:text-white poiret-one-regular" style={{letterSpacing: -1 }}>WHITEWATER TMS</span>
         <img style={{filter: " saturate(1.5)"}} src={neurostar} className="ml-2 h-6 sm:ml-5 " alt="Neurostar logo" />
       </Navbar.Brand>
       <div className="flex md:order-2">
