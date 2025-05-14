13ee53197ced518dae18391aa4a677a9b00a8b7d
Nina-Eld <chang.owen.zhang@gmail.com>
2025-05-13 19:57:56 -0600

    updated social media

--- whitewaterboise/src/components/ContactMe.js
+++ whitewaterboise/src/components/ContactMe.js
@@ -4,37 +4,48 @@ import ROOM from "./statics/office.jpg"
 import PageNavBar from './PageNavBar';
 import PageFooter from './PageFooter';
 import ContactForm from './Contactform';
-
+import { FaFacebook } from 'react-icons/fa';
 const ContactMe = () => {
 
 
   return (
     <><PageNavBar></PageNavBar>
-<div style={{backgroundColor:"#093d4817 "}}className=' px-6 md:px-12 py-12  flex flex-col md:flex-row items-start justify-between relative overflow-hidden'>
-      <section className="contact1-hero w-full md:w-1/2">
-        <div className="contact1-content">
-          <h2 style={{color:" #074cbb "}}>Contact</h2>
-          <p>Phone:<span style={{ marginLeft: "5px" }}>(208) 350-6690</span></p>
-          <p>Fax:<span style={{ marginLeft: "5px" }}>(208) 350-6656</span></p>
-          <br></br>
-          <img src={ROOM} alt="office room" style={{width:"350px",height:"220px", borderRadius:"5%"}}></img>
-        </div>
-      </section>
-      <section className=" w-full md:w-1/2 contact2-hero">
-        <div className="contact2-content">
-          <h2 style={{color:" #074cbb "}} >Location: </h2>
-          <p>908 N Whitewater Park Blvd, Boise, ID 83703</p>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2799.9369209997717!2d-116.23205403613879!3d43.63393518296199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54aef8cc34d7537f%3A0x47448f4ebf5eff8c!2s908%20N%20Whitewater%20Park%20Blvd%2C%20Boise%2C%20ID%2083703!5e0!3m2!1sen!2sus!4v1728517906415!5m2!1sen!2sus"
-            width="360px"
-            height="400px"
-            title='map'
-            style={{ border: 0 ,borderRadius:"5%"}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-      </section>
-</div>
+      <div style={{ backgroundColor: "#093d4817 " }} className=' px-6 md:px-12 py-12  flex flex-col md:flex-row items-start justify-between relative overflow-hidden'>
+        <section className="contact1-hero w-full md:w-1/2">
+          <div className="contact1-content">
+            <h2 style={{ color: " #074cbb " }}>Contact</h2>
+            <p>Phone:<span style={{ marginLeft: "5px" }}>(208) 350-6690</span></p>
+            <p>Fax:<span style={{ marginLeft: "5px" }}>(208) 350-6656</span></p>
+            <p style={{ paddingLeft: '45%' }}>
+              <a
+                href="https://www.facebook.com/people/Whitewater-TMS/61575442227252/#"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Visit our Facebook page"
+                className="text-blue-600 hover:text-blue-800 transition-colors duration-200"
+              >
+                <FaFacebook size={32} />
+              </a>
+            </p>
+            <br></br>
+            <img src={ROOM} alt="office room" style={{ width: "350px", height: "220px", borderRadius: "5%" }}></img>
+          </div>
+        </section>
+        <section className=" w-full md:w-1/2 contact2-hero">
+          <div className="contact2-content">
+            <h2 style={{ color: " #074cbb " }} >Location: </h2>
+            <p>908 N Whitewater Park Blvd, Boise, ID 83703</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2799.9369209997717!2d-116.23205403613879!3d43.63393518296199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54aef8cc34d7537f%3A0x47448f4ebf5eff8c!2s908%20N%20Whitewater%20Park%20Blvd%2C%20Boise%2C%20ID%2083703!5e0!3m2!1sen!2sus!4v1728517906415!5m2!1sen!2sus"
+              width="360px"
+              height="400px"
+              title='map'
+              style={{ border: 0, borderRadius: "5%" }} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </section>
+      </div>
       <section className="contact3-hero">
         <div className="contact3-content">
-          <h2 style={{color:" #074cbb "}}> Parking Instruction</h2>
+          <h2 style={{ color: " #074cbb " }}> Parking Instruction</h2>
           <p>Parking is located in the alley behind the clinic.</p>
 
         </div>
@@ -42,9 +53,9 @@ const ContactMe = () => {
 
 
       <div className="contact-page-hero-borders mt-5 mb-5">
-    <ContactForm></ContactForm>
-</div>
-      
+        <ContactForm></ContactForm>
+      </div>
+
 
       <PageFooter></PageFooter></>
   );
--- whitewaterboise/src/components/PageFooter.js
+++ whitewaterboise/src/components/PageFooter.js
@@ -2,17 +2,30 @@ import React from 'react';
 import "./statics/footer.css"
 import { Footer } from 'flowbite-react';
 import logo from "./logo192.png"
+import { FaFacebook } from 'react-icons/fa';
 const PageFooter = () => {
-    return (
-        <>
-        
+  return (
+    <>
 
-      
+
+
       <div className="contact-info-bar" style={{ backgroundColor: "#243b61", color: "white", display: "flex", alignItems: "center", justifyContent: "space-around", padding: "15px", flexDirection: "column", textAlign: "center" }}>
         <div style={{ backgroundColor: "white", borderRadius: "48px", width: "48px", height: "48px", display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "15px" }}>
           <img src={logo} style={{ height: "34px" }} alt="Whitewater TMS logo" />
         </div>
         <ul className='ml-0 mt-0 items-center justify-center flex flex-column' style={{ margin: 0, padding: 0, listStyle: "none" }}>
+          <li style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%", marginBottom: "10px" }}> 
+            <a
+              href="https://www.facebook.com/people/Whitewater-TMS/61575442227252/#"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Visit our Facebook page"
+              className="text-blue-600 hover:text-blue-800 transition-colors duration-200"
+            >
+              <FaFacebook size={32} />
+            </a>
+          </li>
+
           <li style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%", marginBottom: "10px" }}>
             <img data-at="image" alt="address" src="//v.fastcdn.co/u/89b2219b/62086539-0-bx-map-pin.png" data-src="//v.fastcdn.co/u/89b2219b/62086539-0-bx-map-pin.png" data-retina-src="//v.fastcdn.co/u/89b2219b/62086539-0-bx-map-pin.png" srcSet="//v.fastcdn.co/u/89b2219b/62086539-0-bx-map-pin.png 2x" style={{ marginRight: "5px", height: "20px" }} />
             <span>908 N Whitewater Park Blvd, Boise, ID 83703</span>
@@ -21,30 +34,30 @@ const PageFooter = () => {
             <img data-at="image" alt="phone number" src="//v.fastcdn.co/u/89b2219b/62104854-0-Group.png" data-src="//v.fastcdn.co/u/89b2219b/62104854-0-Group.png" data-retina-src="//v.fastcdn.co/u/89b2219b/62104854-0-Group.png" srcSet="//v.fastcdn.co/u/89b2219b/62104854-0-Group.png 2x" style={{ marginRight: "5px", height: "20px" }} />
             <span>(208) 350-6690</span>
           </li>
-          <li  style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%" }}>
-            <img  data-at="image" alt="" src="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png" data-src="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png" data-retina-src="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png" srcSet="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png 2x" style={{ marginRight: "5px", height: "20px" }} />
+          <li style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100%" }}>
+            <img data-at="image" alt="" src="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png" data-src="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png" data-retina-src="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png" srcSet="//v.fastcdn.co/u/89b2219b/62104846-0-Vector.png 2x" style={{ marginRight: "5px", height: "20px" }} />
             <span> info@whitewatertms.com</span>
           </li>
         </ul>
       </div>
-  
-            <Footer container className="mt-auto bg-gray-900 py-8 rounded-none">
-                <div className="w-full text-center text-white">
-                    < Footer.Copyright href="#" by="Whitewater TMS" year={2025} style={{marginTop:"10px"}} />
-                    <Footer.LinkGroup >
-                        <Footer.Link href="about" className="text-white hover:text-cyan-300">
-                            About Us
-                        </Footer.Link>
-                        <Footer.Link href="privacy" className="text-white hover:text-cyan-300">
-                            Privacy Policy
-                        </Footer.Link>
-                        <Footer.Link href="termsofservice" className="text-white hover:text-cyan-300">
-                            Terms of Service
-                        </Footer.Link>
-                    </Footer.LinkGroup>
-                </div>
-            </Footer></>
-    );
+
+      <Footer container className="mt-auto bg-gray-900 py-8 rounded-none">
+        <div className="w-full text-center text-white">
+          < Footer.Copyright href="#" by="Whitewater TMS" year={2025} style={{ marginTop: "10px" }} />
+          <Footer.LinkGroup >
+            <Footer.Link href="about" className="text-white hover:text-cyan-300">
+              About Us
+            </Footer.Link>
+            <Footer.Link href="privacy" className="text-white hover:text-cyan-300">
+              Privacy Policy
+            </Footer.Link>
+            <Footer.Link href="termsofservice" className="text-white hover:text-cyan-300">
+              Terms of Service
+            </Footer.Link>
+          </Footer.LinkGroup>
+        </div>
+      </Footer></>
+  );
 };
 
 export default PageFooter;
